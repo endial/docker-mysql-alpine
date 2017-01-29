743e78f3e3224c6846915423849312c70cfb883c
Endial Fang <endial@EndialderMBP.local>
2017-01-29 19:35:31 +0800

    增加默认创建数据存储目录

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,8 @@ if [ -d /srv/data/mysql ]; then
 else
   echo "[i] MySQL data directory not found, creating initial DBs"
 
+  mkdir -p /srv/data/mysql
+
   mysql_install_db --user=root > /dev/null
 
   if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
